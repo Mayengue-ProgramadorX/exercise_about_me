a2821170e929025a3766567997ff8ea3b5a715f9
Jose Mayengue <josemayengue51@gmail.com>
2026-08-14 17:10:12 -0700

    Exercise about me

---
+++ about_me.py
@@ -0,0 +1,22 @@
+#Customize the display with "===" just to make it look nice.
+print("="*44)
+print("Student Profile".center(44)) # center() used to center text 
+print("="*44)
+
+#describing variable names 
+first_name = "Jose"
+last_name = "Antonio"
+age = 28
+city = "Portland"
+previous_career = "T3 operate machine"
+reason_to_learn_code = "I've always loved programming."
+fun_fact = "I created my own app to conjugate English verbs."
+
+#display names 
+print(f"\nName: {first_name} {last_name}")
+print("Age: {28}")
+print("City: {city}")
+print("Previous Career: {previous_career}")
+print("Why I'm learning to code: {reason_to_learn_code}")
+print("Fun fact: {fun_fact}")
+print("="*44)
